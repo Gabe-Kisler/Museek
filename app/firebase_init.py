9b48f7afe1e441f5912db1f333c9ae5733560330
Gabe <gabrielkisler@earthlink.net>
2025-07-17 12:27:37 -0500

    fixed firebase init issue

--- app/firebase_init.py
+++ app/firebase_init.py
@@ -1,9 +1,14 @@
+import os
+import json
 import firebase_admin
 from firebase_admin import auth, credentials, firestore
 
 def firebase_init():
     if not firebase_admin._apps:
-        cred = credentials.Certificate('app/credentials/serviceKey.json')
+        service_key_json = os.getenv("FIREBASE_SERVICE_KEY_JSON")
+        if service_key_json:
+            # Load credentials from JSON string in env var
+            service_account_info = json.loads(service_key_json)
+            cred = credentials.Certificate(service_account_info)
+
         firebase_admin.initialize_app(cred)
-
-
